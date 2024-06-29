024251ac2c7a850a5c69dfd0d865e0ad6fd4f946
Prashant Gaware <prashantgaware8@gmail.com>
2024-06-29 19:40:42 +0530

    IOCProj01-SetterInjection

---
+++ IOCProj01-SetterInjection/src/main/java/com/nt/beans/WishMessageGenerator.java
@@ -0,0 +1,26 @@
+package com.nt.beans;
+
+import java.util.Date;
+
+public class WishMessageGenerator {
+	
+	private Date date;
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String generateMessage(String user) {
+		int hour = date.getHours();
+		
+		if(hour < 12) {
+			return "Good Morning " + user;
+		} else if(hour < 16) {
+			return "Good Afternoon " + user;
+		} else if (hour < 20) {
+			return "Good Evening " + user;
+		} else {
+			return "Good Night " + user;
+		}
+	}
+}
