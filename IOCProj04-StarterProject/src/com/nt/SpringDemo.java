d2b19e7f0a33727ecb5065f73d39406057515c67
Prashant Gaware <prashantgaware8@gmail.com>
2024-07-27 11:37:50 +0530

    SpringDemo

--- IOCProj04-StarterProject/src/com/nt/SpringDemo.java
+++ IOCProj04-StarterProject/src/com/nt/SpringDemo.java
@@ -5,13 +5,16 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class SpringDemo {
 	public static void main(String[] args) {
-		BeanFactory factory = new FileSystemXmlApplicationContext("D:\\Practice\\SpringBootProjects\\IOCProj04-StarterProject\\bean.xml");
+		BeanFactory factory = 
+				new FileSystemXmlApplicationContext("D:\\Practice\\SpringBootProjects\\IOCProj04-StarterProject\\bean.xml");
 		
 		Object obj = factory.getBean("student1");
-		
 		Student student = (Student) obj;
-		
 		student.printStudent();
-		
+		System.out.println();
+		obj = factory.getBean("emp1");
+		Employee emp = (Employee) obj;
+		System.out.println("Employee ID : " + emp.empId);
+		System.out.println("Employee ID : " + emp.empName);
 	}
 }
