249dde63231784788ee6b0cf59320b8a5f8068b6
Prashant Gaware <prashantgaware8@gmail.com>
2025-02-11 23:49:06 +0530

    spring bean life cycle

---
+++ IOCProj27-SpringBeanLifecycle/src/test/java/com/example/chatapp/IocProj27SpringBeanLifecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocProj27SpringBeanLifecycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
