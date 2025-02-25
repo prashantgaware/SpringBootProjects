c805f99fac317ac6d6faa18fca0c1a4254dc0d8f
Prashant Gaware <prashantgaware8@gmail.com>
2025-02-25 22:26:15 +0530

    BootJPAProj08-Query-Annotation

---
+++ BootJPAProj08-Query-Annotation/src/main/java/com/nt/repo/IMovieRepo.java
@@ -0,0 +1,11 @@
+package com.nt.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nt.entity.Movie;
+
+public interface IMovieRepo extends JpaRepository<Movie, Integer> {
+	
+}
