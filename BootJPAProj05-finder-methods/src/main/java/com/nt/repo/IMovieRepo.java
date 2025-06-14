c9fdb46a9f5ca38fd91baca2bb25e4d5cf946cd4
Prashant Gaware <prashantgaware8@gmail.com>
2025-06-14 20:25:24 +0530

    finder methods

--- BootJPAProj05-finder-methods/src/main/java/com/nt/repo/IMovieRepo.java
+++ BootJPAProj05-finder-methods/src/main/java/com/nt/repo/IMovieRepo.java
@@ -9,5 +9,35 @@ import com.nt.entity.Movie;
 public interface IMovieRepo extends JpaRepository<Movie, Integer> {
 	
 	List<Movie> findByMovieName(String name);
+	
+	List<Movie> findByMovieRating(Float rating);
+	
+	List<Movie> findByReleaseYear(String year);
+	
+	List<Movie> findByMovieNameAndMovieRating(String name, Float rating);
+	
+	List<Movie> findByMovieNameOrReleaseYear(String name, String year);
+	
+	List<Movie> findByMovieNameLike(String pattern);
+	
+	List<Movie> findByMovieNameNotLike(String pattern);
+	
+	List<Movie> findByMovieNameStartingWith(String prefix);
+	
+	List<Movie> findByMovieNameEndingWith(String suffix);
+	
+	List<Movie> findByMovieNameContaining(String substring);
+	
+	List<Movie> findByMovieNameNot(String name);
+	
+	List<Movie> findByMovieRatingBetween(Float start, Float end);
+	
+	List<Movie> findByMovieRatingLessThan(Float rating);		
+	
+	List<Movie> findByMovieRatingGreaterThan(Float rating);
+	
+	List<Movie> findAll();
+	
+	long count();
 
 }
--- BootJPAProj05-finder-methods/src/main/java/com/nt/runner/Runner.java
+++ BootJPAProj05-finder-methods/src/main/java/com/nt/runner/Runner.java
@@ -17,6 +17,36 @@ public class Runner implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		repo.findByMovieName("Joker").forEach(System.out::println);
 		
+		System.out.println("Movie by rating : ");
+		repo.findByMovieRating(8.5f).forEach(System.out::println);
+		System.out.println("Movie by year : ");
+		repo.findByReleaseYear("2019").forEach(System.out::println);
+		System.out.println("Movie by name and rating : ");
+		repo.findByMovieNameAndMovieRating("Joker", 8.5f).forEach(System.out::println);
+		System.out.println("Movie by name or year : ");
+		repo.findByMovieNameOrReleaseYear("Joker", "2019").forEach(System.out::println);
+		System.out.println("Movie by name like : ");
+		repo.findByMovieNameLike("%Joker%").forEach(System.out::println);
+		System.out.println("Movie by name not like : ");
+		repo.findByMovieNameNotLike("%Joker%").forEach(System.out::println);
+		System.out.println("Movie by name starting with : ");
+		repo.findByMovieNameStartingWith("J").forEach(System.out::println);
+		System.out.println("Movie by name ending with : ");
+		repo.findByMovieNameEndingWith("ker").forEach(System.out::println);
+		System.out.println("Movie by name containing : ");
+		repo.findByMovieNameContaining("oke").forEach(System.out::println);
+		System.out.println("Movie by name not : ");
+		repo.findByMovieNameNot("Joker").forEach(System.out::println);
+		System.out.println("Movie by rating between : ");
+		repo.findByMovieRatingBetween(8.0f, 9.0f).forEach(System.out::println);
+		System.out.println("Movie by rating less than : ");
+		repo.findByMovieRatingLessThan(8.5f).forEach(System.out::println);
+		System.out.println("Movie by rating greater than : ");
+		repo.findByMovieRatingGreaterThan(8.5f).forEach(System.out::println);
+		System.out.println("All movies : ");
+		repo.findAll().forEach(System.out::println);
+		System.out.println("Count of movies : " + repo.count());
+		
 	}
-
+	
 }
