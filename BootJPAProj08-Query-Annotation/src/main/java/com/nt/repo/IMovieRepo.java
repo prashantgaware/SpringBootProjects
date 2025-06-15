cf00f55e3025a53049f1809d9e96986de17d520a
Prashant Gaware <prashantgaware8@gmail.com>
2025-06-15 19:02:51 +0530

    jpql/hql query using @Query annotation

--- BootJPAProj08-Query-Annotation/src/main/java/com/nt/repo/IMovieRepo.java
+++ BootJPAProj08-Query-Annotation/src/main/java/com/nt/repo/IMovieRepo.java
@@ -3,9 +3,33 @@ package com.nt.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nt.entity.Movie;
 
 public interface IMovieRepo extends JpaRepository<Movie, Integer> {
+	
+	@Query("Select m FROM Movie m WHERE m.movieId BETWEEN :min AND :max")
+	List<Movie> searchMoviesByIdRange(int min, int max);
+	
+	@Query("Select m FROM Movie m WHERE m.movieRating >= :minRating AND m.movieRating <= :maxRating")
+	List<Movie> searchMoviesByRatingRange(float minRating, float maxRating);
+	
+	@Query("Select m FROM Movie m WHERE m.movieName LIKE CONCAT(:prefix, '%')")
+	List<Movie> getMoviesStartingWith(String prefix);
+	
+	@Query("Select m FROM Movie m")
+	List<Movie> getAllMovies();
+	
+	@Query("Select count(m) FROM Movie m")
+	int getCountOfMovie();
+	
+	// this two annotations are required when we are updating or inserting records 
+	@Modifying
+	@Transactional
+	@Query("UPDATE Movie m SET m.movieRating = :rating WHERE m.movieId = :id")
+	int updateMovie(int id, float rating);
 	
 }
--- BootJPAProj08-Query-Annotation/src/main/java/com/nt/runner/Runner.java
+++ BootJPAProj08-Query-Annotation/src/main/java/com/nt/runner/Runner.java
@@ -15,6 +15,32 @@ public class Runner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		try {
+			System.out.println("------------------------------------------------");
+			repo.searchMoviesByIdRange(117, 240).forEach(System.out::println);
+			
+			System.out.println("-------------------------------------------------");
+			repo.searchMoviesByRatingRange(8.5f, 9.5f).forEach(System.out::println);
+			
+			System.out.println("-------------------------------------------------");
+			repo.getMoviesStartingWith("T").forEach(System.out::println);
+			
+			System.out.println("Getting all the movies:");
+			repo.getAllMovies().forEach(System.out::println);
+			
+			System.out.println("-------------------------------------------------");
+			System.out.println("Count of movies: " + repo.getCountOfMovie());
+			
+			System.out.println("-------------------------------------------------");
+			System.out.println("Updating movie with ID 210 to rating 9.0");
+			System.out.println("Rows Affected: " + repo.updateMovie(210, 9.3f));
+			repo.findById(210).ifPresentOrElse(
+					movie -> System.out.println("Movie updated: " + movie),
+					() -> System.out.println("Movie with ID 210 not found."));
+			System.out.println();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
